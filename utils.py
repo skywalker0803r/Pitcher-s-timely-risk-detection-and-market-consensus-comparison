e015cf6cbff38b51c81942708b2f67d60b2004c0
skywalker0803r <skywalker0803r@gmail.com>
2025-05-31 21:55:46 +0800

    update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import mediapipe as mp
+import numpy as np
 
 def get_landmark_vector(lm, index):
     if isinstance(lm, list):
@@ -18,3 +20,27 @@ def calculate_angle(a, b, c):
         return np.array([calculate_angle_single_frame(a[i], b[i], c[i]) for i in range(len(a))])
     else:
         return calculate_angle_single_frame(a, b, c)
+
+def extract_pose_from_frame(image,mp_pose,mp_drawing,pose_detector):
+    """提取 3D pose 並在原圖上畫出關節骨架"""
+    results = pose_detector.process(image)
+    if results.pose_landmarks:
+        # 繪製骨架
+        mp_drawing.draw_landmarks(
+            image, 
+            results.pose_landmarks, 
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
+            connection_drawing_spec=mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2)
+        )
+        # 印出所有關鍵點的 x, y, z
+        for i, landmark in enumerate(results.pose_landmarks.landmark):
+            print(f"Landmark {i}: x={landmark.x:.3f}, y={landmark.y:.3f}, z={landmark.z:.3f}, visibility={landmark.visibility:.2f}")
+
+        # 提取 99 維 pose 特徵
+        keypoints = []
+        for lm in results.pose_landmarks.landmark:
+            keypoints.extend([lm.x, lm.y, lm.z])
+        return np.array(keypoints)
+    else:
+        return None
